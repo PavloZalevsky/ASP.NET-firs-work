8f5c69bc7ba31444b3be02c686b7958a7f0c7f64
pasha <pavlo.zalevsky@gmail.com>
2016-04-18 13:54:07 +0300

    ADD: 3 block

---
+++ WebApplication1/WebApplication1/page/js/script.js
@@ -0,0 +1,20 @@
+﻿function OnBtnShowClick()
+{
+    Hide();
+    document.getElementById("show").style.display = "block";
+}
+function OnBtnAddClick() {
+    Hide();
+    document.getElementById("add").style.display = "block";
+}
+function OnBtnRecmoveClick() {
+    Hide();
+    document.getElementById("remove").style.display = "block";
+}
+
+
+function Hide() {
+    document.getElementById("show").style.display = "none";
+    document.getElementById("add").style.display = "none";
+    document.getElementById("remove").style.display = "none";
+}
