58dd25c3828341c916e88adbb30cd43cdd1f1dc3
pasha <pavlo.zalevsky@gmail.com>
2016-04-18 18:38:12 +0300

    fix

--- WebApplication1/WebApplication1/page/js/script.js
+++ WebApplication1/WebApplication1/page/js/script.js
@@ -1,6 +1,6 @@
-﻿function OnBtnShowClick()
-{
+﻿function OnBtnShowClick() {
     Hide();
+    Show();
     document.getElementById("show").style.display = "block";
 }
 function OnBtnAddClick() {
@@ -11,6 +11,36 @@ function OnBtnRecmoveClick() {
     Hide();
     document.getElementById("remove").style.display = "block";
 }
+
+var uri = 'api/products';
+function Show() {
+ 
+    $(document).ready(function () {
+        $.getJSON(uri)
+             .done(function (data) {
+                 $.each(data, function (key, item) {
+                     // Add a list item for the product.
+                     $('<li>', { text: formatItem(item) }).appendTo($('#products'));
+                 })
+             });
+    });
+}
+
+function find() {
+    var id = $('#prodId').val();
+    $.getJSON(uri + '/' + id)
+        .done(function (data) {
+            $('#product').text(formatItem(data));
+        })
+        .fail(function (jqXHR, textStatus, err) {
+            $('#product').text('Error: ' + err);
+        });
+}
+function formatItem(item) {
+    return item.Name + ': $' + item.Price;
+}
+
+
 
 
 function Hide() {
